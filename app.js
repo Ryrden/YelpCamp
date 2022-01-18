9bea011fdd956bdc770ea04cb1a69ccd501356fb
Ryan Souza <ryansateles@gmail.com>
2022-01-18 01:56:26 -0300

    Campground Delete done

--- app.js
+++ app.js
@@ -52,15 +52,16 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
 app.put("/campgrounds/:id", async (req, res) => {
 	const { id } = req.params;
 	const campground = await Campground.findByIdAndUpdate(id, {
-		...req.body.campground
+		...req.body.campground,
 	});
 	res.redirect(`/campgrounds/${campground._id}`);
 });
 
-
-
-
-
+app.delete("/campgrounds/:id", async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id);
+	res.redirect("/campgrounds");
+});
 
 app.listen(3000, () => {
 	console.log("Server is Online on port 3000");
