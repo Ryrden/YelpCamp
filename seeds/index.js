7bdb5a2a4024e5f492b82cfebd533f289584c29f
Ryan Souza <ryansateles@gmail.com>
2022-01-19 11:12:14 -0300

    Adding images using seed algorithm and Unsplash API

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
-const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers')
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -11,20 +11,25 @@ db.once("open", () => {
 	console.log("Database connected");
 });
 
-const sample = (array) => array[Math.floor(Math.random()*array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
 	await Campground.deleteMany({});
 	for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground ({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save()
-    }
+		const random1000 = Math.floor(Math.random() * 1000);
+		const price = Math.floor(Math.random() * 20) + 10;
+		const camp = new Campground({
+			location: `${cities[random1000].city}, ${cities[random1000].state}`,
+			title: `${sample(descriptors)} ${sample(places)}`,
+			image: "https://source.unsplash.com/collection/483251",
+			description:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique earum accusantium doloremque facere beatae nobis ex! Sit rem explicabo ea a corporis eum animi labore doloremque, aliquid modi saepe exercitationem?",
+			price
+		});
+		await camp.save();
+	}
 };
 
 seedDB().then(() => {
-    mongoose.connection.close()
-})
+	mongoose.connection.close();
+});
