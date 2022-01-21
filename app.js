9fdc9cfea4f9b93b6d02805fb6a2810d546b8083
Ryan Souza <ryansateles@gmail.com>
2022-01-21 11:55:53 -0300

    Handling Async Erros and Defining ExpressError Class

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const ejsMate = require('ejs-mate')
+const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/CatchAsync");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -15,11 +16,11 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({ extended: true })); //to send forms
+app.use(express.urlencoded({extended: true})); //to send forms
 app.use(methodOverride("_method")); //to do "fakes" PUT,DELETE, etc
 
 app.get("/", (req, res) => {
@@ -28,41 +29,46 @@ app.get("/", (req, res) => {
 
 app.get("/campgrounds", async (req, res) => {
 	const campgrounds = await Campground.find({});
-	res.render("campgrounds/index", { campgrounds });
+	res.render("campgrounds/index", {campgrounds});
 });
 
 app.get("/campgrounds/new", (req, res) => {
 	res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-	const campground = new Campground(req.body.campground);
-	await campground.save();
-	res.redirect(`/campgrounds/${campground._id}`);
-});
+app.post("/campgrounds", catchAsync(async (req, res, next) => {
+		const campground = new Campground(req.body.campground);
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id", catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/show", { campground });
-});
+	res.render("campgrounds/show", {campground});
+}));
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
+app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
-	res.render("campgrounds/edit", { campground });
-});
+	res.render("campgrounds/edit", {campground});
+}));
 
-app.put("/campgrounds/:id", async (req, res) => {
-	const { id } = req.params;
+app.put("/campgrounds/:id", catchAsync(async (req, res) => {
+	const {id} = req.params;
 	const campground = await Campground.findByIdAndUpdate(id, {
 		...req.body.campground,
 	});
 	res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete("/campgrounds/:id", async (req, res) => {
-	const { id } = req.params;
+app.delete("/campgrounds/:id", catchAsync(async (req, res) => {
+	const {id} = req.params;
 	await Campground.findByIdAndDelete(id);
 	res.redirect("/campgrounds");
+}));
+
+app.use(async (err, req, res, next) => {
+	res.send("oh boy something went wrong");
 });
 
 app.listen(3000, () => {
