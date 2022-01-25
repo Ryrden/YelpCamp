5f283efde2957c484cb41279e6fd222ebbf92b90
Ryan Souza <ryansateles@gmail.com>
2022-01-25 15:48:21 -0300

    review delete implemented

--- app.js
+++ app.js
@@ -60,7 +60,6 @@ app.get("/campgrounds/new", (req, res) => {
 });
 
 app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
-	// if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 	const campground = new Campground(req.body.campground);
 	await campground.save();
 	res.redirect(`/campgrounds/${campground._id}`);
@@ -68,7 +67,7 @@ app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) =
 );
 
 app.get("/campgrounds/:id", catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id);
+	const campground = await Campground.findById(req.params.id).populate('reviews');
 	res.render("campgrounds/show", {campground});
 }));
 
@@ -98,6 +97,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req,res) 
 	await review.save();
 	await campground.save()
 	res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req,res) => {
+	const {id, reviewId} = req.params;
+	await Campground.findByIdAndUpdate(id,{$pull: { reviews: reviewId }});
+	await Review.findByIdAndDelete(reviewId);
+	res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', (req,res,next)=> {
