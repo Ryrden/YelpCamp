a27b32e492c2609b196b3bb37377fc68baed8b3c
Ryan Souza <ryansateles@gmail.com>
2022-01-27 13:48:26 -0300

    cookies session setted

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require('express-session')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require("method-override");
 
@@ -26,12 +27,26 @@ app.use(express.urlencoded({extended: true})); //to send forms
 app.use(methodOverride("_method")); //to do "fakes" PUT,DELETE, etc
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+const sessionConfig = {
+	secret: 'ThisShouldBeaBetterSecrete!',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
+}
+
+app.use(session(sessionConfig))
+
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
-app.get("/", (req, res) => {
-	res.render("home");
-});
+// app.get("/", (req, res) => {
+// 	res.render("home");
+// });
 
 app.all('*', (req,res,next)=> {
 	next(new ExpressError('Page not Found', 404))
