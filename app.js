04508feba41cdf47c4321957fbb9039320a3b5d1
Ryan Souza <ryansateles@gmail.com>
2022-01-27 14:44:04 -0300

    Set up Flash and defining an Error and Succes Partial to it

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const session = require('express-session')
+const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require("method-override");
 
@@ -40,13 +41,20 @@ const sessionConfig = {
 }
 
 app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req,res,next) => {
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error')
+	next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
-// app.get("/", (req, res) => {
-// 	res.render("home");
-// });
+app.get("/", (req, res) => {
+	res.render("home");
+});
 
 app.all('*', (req,res,next)=> {
 	next(new ExpressError('Page not Found', 404))
--- models/campground.js
+++ models/campground.js
@@ -16,7 +16,7 @@ const CampgroundSchema = new Schema({
     ]
 })
 
-CampgroundSchema.post('findOneAndDelete', async function(){
+CampgroundSchema.post('findOneAndDelete', async function(doc){
     if (doc){
         await Review.deleteMany({
             _id: {
