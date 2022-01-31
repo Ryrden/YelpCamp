933fb977a8f78fe3e7aca92965759f157d5e39e6
Ryan Souza <ryansateles@gmail.com>
2022-01-31 16:57:51 -0300

    User model and Passport package config Done!

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require("method-override");
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
@@ -28,25 +31,36 @@ app.use(express.urlencoded({extended: true})); //to send forms
 app.use(methodOverride("_method")); //to do "fakes" PUT,DELETE, etc
 app.use(express.static(path.join(__dirname, 'public')))
 
-
+const WeekMiliSecond = 1000 * 60 * 60 * 24 * 7
 const sessionConfig = {
-	secret: 'ThisShouldBeaBetterSecrete!',
+	secret: 'ThisShouldBeaBetterSecret!',
 	resave: false,
 	saveUninitialized: true,
 	cookie: {
 		httpOnly: true,
-		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-		maxAge: 1000 * 60 * 60 * 24 * 7
+		expires: Date.now() + WeekMiliSecond,
+		maxAge: WeekMiliSecond
 	}
 }
 
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req,res,next) => {
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error')
 	next();
+})
+
+app.get('/fakeUser', async (req,res) => {
+	const user = new User({email: ''})
 })
 
 app.use('/campgrounds', campgrounds)
