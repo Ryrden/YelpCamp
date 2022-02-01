fc9f730a49e2041390a425b99a59403bc570a9a0
Ryan Souza <ryansateles@gmail.com>
2022-02-01 13:36:13 -0300

    isLoggedIn Middleware, add logout and create HTML links

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 const User = require('../models/user')
 const { route } = require('./campgrounds')
 
@@ -18,8 +19,21 @@ router.post('/register', async(req,res) => {
         req.flash('error', e.message)
         res.redirect('register')
     }
-    
+})
 
+router.get('/login', (req,res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req,res) => {
+    req.flash('success', 'welcome back!')
+    res.redirect('/campgrounds')
+})
+
+router.get('/logout', (req,res) => {
+    req.logout();
+    req.flash('success', "Goodbye")
+    res.redirect('/campgrounds')
 })
 
 module.exports = router
