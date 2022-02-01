a16f835a8772c7199633e0487e48b26f9e0b32ad
Ryan Souza <ryansateles@gmail.com>
2022-02-01 17:41:54 -0300

    Reviews Permissions

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require('./schemas.js')
 const ExpressError = require('./utils/ExpressError')
 const Campground = require("./models/campground");
+const Review = require('./models/review')
 
 module.exports.isLoggedIn = (req,res,next) => {
     if (!req.isAuthenticated()){
@@ -28,6 +29,7 @@ module.exports.isAuthor = async (req,res,next) => {
 		req.flash('error', 'You do not have permission to do that')
 		return res.redirect(`/campgrounds/${id}`)
 	}
+	next();
 }
 
 module.exports.validateReview = (req,res,next) => {
@@ -38,4 +40,14 @@ module.exports.validateReview = (req,res,next) => {
 	} else{
 		next()
 	}
+}
+
+module.exports.isReviewAuthor = async (req,res,next) => {
+	const { id, reviewId } = req.params
+	const review = await Review.findById(reviewId)
+	if(!review.author.equals(req.user._id)){
+		req.flash('error', 'You do not have permission to do that')
+		return res.redirect(`/campgrounds/${id}`)
+	}
+	next();
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,8 +22,13 @@ router.post("/", isLoggedIn , validateCampground, catchAsync(async (req, res, ne
 	})
 );
 
-router.get("/:id", isAuthor ,catchAsync(async (req, res) => {
-	const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+router.get("/:id", catchAsync(async (req, res) => {
+	const campground = await Campground.findById(req.params.id).populate({
+		path:'reviews',
+		populate: {
+			path:'author'
+		}
+	}).populate('author');
 	if (!campground){
 		req.flash('error', 'Cannot find that campground!')
 		return res.redirect('/campgrounds')
@@ -41,7 +46,7 @@ router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(async (req, res) => {
 	res.render("campgrounds/edit", {campground});
 }));
 
-router.put("/:id", isLoggedIn, validateCampground, isAuthor, catchAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
 	const {id} = req.params;
 	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
 	req.flash('success', 'Successfully updated campground!')
