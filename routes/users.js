fc21929607ae2ab605baa29031dba975519a8fa8
Ryan Souza <ryansateles@gmail.com>
2022-02-02 11:15:34 -0300

    resctructure routes by endpoints using router.route()

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,16 @@ const passport = require('passport')
 const User = require('../models/user')
 const users = require('../controllers/users')
 
-router.get('/register', users.renderRegister)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(users.register)
 
-router.post('/register', users.register)
-
-router.get('/login', users.renderLogin)
-
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login'
+    }), users.login)
 
 router.get('/logout', users.logout)
 
